1a827444f39cf6de570e8a9dfeae1048ea9c1e21
Faizal Luthfi <zalluth@gmail.com>
2018-10-11 03:25:46 +0700

    refactor: add "deleted" column to "materials" table

--- migrations/20181002074540_create_materials_table.js
+++ migrations/20181002074540_create_materials_table.js
@@ -3,6 +3,7 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('materials', function(table) {
     table.increments();
     table.string('name');
+    table.boolean('deleted');
   });
 };
 
