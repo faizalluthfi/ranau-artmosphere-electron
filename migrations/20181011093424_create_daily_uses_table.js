f36c19c377048477385d6978608dea20e7a68492
Faizal Luthfi <zalluth@gmail.com>
2018-10-11 09:47:34 +0700

    refactor: create daily_uses and daily_materials_uses tables

---
+++ migrations/20181011093424_create_daily_uses_table.js
@@ -0,0 +1,16 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('daily_uses', function(table) {
+    table.increments();
+    table.integer('created_at');
+  }).createTable('daily_materials_uses', function(table) {
+    table.increments();
+    table.integer('daily_use_id').references('daily_uses.id');
+    table.integer('material_id');
+    table.integer('nominal');
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('daily_uses').dropTable('daily_materials_uses');
+};
